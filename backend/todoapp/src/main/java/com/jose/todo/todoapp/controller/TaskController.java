2da931666466fa63795b189cd3e9c70958e6c95e
Jose Valdez <jvaldez-fernandez@csumb.edu>
2026-03-20 14:33:55 -0700

    Updated Task Controller

--- backend/todoapp/src/main/java/com/jose/todo/todoapp/controller/TaskController.java
+++ backend/todoapp/src/main/java/com/jose/todo/todoapp/controller/TaskController.java
@@ -1,13 +1,40 @@
 package com.jose.todo.todoapp.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jose.todo.todoapp.model.Task;
+import com.jose.todo.todoapp.service.TaskService;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
-    
-    @GetMapping("/")
-    public String hello(){
-        return "Hello There";
+
+    private final TaskService service;
+
+    public TaskController(TaskService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Task> getTasks(){
+        return service.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return service.createTask(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        service.deleteTask(id);
     }
 }
