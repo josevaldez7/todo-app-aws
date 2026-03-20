612666a43871cece17e1a3043fbb71a3ff9f27cf
Jose Valdez <jvaldez-fernandez@csumb.edu>
2026-03-20 13:09:51 -0700

    Initial Spring Boot Setup, created sample hello path

---
+++ backend/todoapp/src/main/java/com/jose/todo/todoapp/TodoappApplication.java
@@ -0,0 +1,13 @@
+package com.jose.todo.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+}
