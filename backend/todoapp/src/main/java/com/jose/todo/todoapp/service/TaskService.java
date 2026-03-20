8098d09d467ebcc6f6de81f71192e34b0ccc8250
Jose Valdez <jvaldez-fernandez@csumb.edu>
2026-03-20 14:30:10 -0700

    Created Task Service

---
+++ backend/todoapp/src/main/java/com/jose/todo/todoapp/service/TaskService.java
@@ -0,0 +1,29 @@
+package com.jose.todo.todoapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jose.todo.todoapp.model.Task;
+import com.jose.todo.todoapp.repository.TaskRepository;
+
+@Service
+public class TaskService {
+    private final TaskRepository repo;
+
+    public TaskService(TaskRepository repo){
+        this.repo = repo;
+    }
+
+    public List<Task> getAllTasks(){
+        return repo.findAll();
+    }
+
+    public Task createTask(Task task){
+        return repo.save(task);
+    }
+
+    public void deleteTask(Long id){
+        repo.deleteById(id);
+    }
+}
